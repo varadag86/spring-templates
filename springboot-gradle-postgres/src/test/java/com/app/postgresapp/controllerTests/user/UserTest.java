24157f1271e0c7bd451c60a8447cd5ab66013a95
Varad <py.varad@gmail.com>
2023-03-07 08:06:30 +0530

    Added test cases

--- springboot-gradle-postgres/src/test/java/com/app/postgresapp/controllerTests/user/UserTest.java
+++ springboot-gradle-postgres/src/test/java/com/app/postgresapp/controllerTests/user/UserTest.java
@@ -1,9 +1,8 @@
 package com.app.postgresapp.controllerTests.user;
 
+import com.app.postgresapp.domain.User;
 import com.app.postgresapp.repository.UserRepository;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,14 +25,38 @@ public class UserTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void setupTest() {
+        User admin = new User();
+        admin.setName("John Doe");
+        admin.setExperience(14);
+        admin.setEmailAddress("john.doe@test.com");
+        this.userRepository.saveAndFlush(admin);
+
+        User staff = new User();
+        staff.setName("John Doe");
+        staff.setExperience(14);
+        staff.setEmailAddress("john.doe@test.com");
+        this.userRepository.saveAndFlush(staff);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        this.userRepository.deleteAll();
+    }
 
     @Test
     void get_all_users() throws Exception {
+
         mockMvc.perform(get("/api/users")
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$").isArray())
-                .andExpect(content().json("[]"));
+                .andExpect(content().json("[{\"id\":1,\"name\":\"John Doe\",\"emailAddress\":\"john.doe@test.com\",\"experience\":14},{\"id\":2,\"name\":\"John Doe\",\"emailAddress\":\"john.doe@test.com\",\"experience\":14}]"));
     }
+    
 }
